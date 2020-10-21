21713f895cd465789178a4e3406d6956061bd1df
futrueface <381781732@qq.com>
2020-10-21 12:46:32 +0800

    修改了游戏的逻辑，胜负开始随机

--- 20201020/main.py
+++ 20201020/main.py
@@ -2,23 +2,27 @@ import random
 
 
 def fight(x_hp, x_power, y_hp, y_power):
-    my_final_hp = x_hp - y_power
-    enemy_final_hp = y_hp - x_power
-
-    if my_final_hp > enemy_final_hp:
-        return "we win"
-    elif my_final_hp < enemy_final_hp:
-        return 'we false'
-    else:
-        return '平局'
+    my_final_hp = x_hp
+    enemy_final_hp = y_hp
+    while True:
+        my_final_hp = my_final_hp - y_power
+        enemy_final_hp = enemy_final_hp - x_power
+        if my_final_hp > 0 and enemy_final_hp > 0:
+            print(f'敌人干掉我{my_final_hp}的血量，我还在坚持！我方血量{x_hp-my_final_hp}')
+        elif my_final_hp <= 0:
+            print(f'敌人太厉害，我被干掉了，但是敌人也被我重创，敌人的血量还剩：{enemy_final_hp}')
+            return 'we false'
+        elif enemy_final_hp <= 0:
+            print(f'敌人虽然勇猛，但是在我方坚强抵抗下，被杀的丢盔弃甲！我方血量{my_final_hp}')
+            return "we win"
 
 
 if __name__ == "__main__":
-    enemy_hp = [random.randint(800, 1000) for _ in range(4)]
 
     # enemy_hp = [x for x in range(100, 1100)]
     roundq = 1
     while True:
+        enemy_hp = [random.randint(800, 1000) for _ in range(4)]
         # a = random.choice(enemy_hp)
         # b = random.choice(enemy_hp)
         # c = random.choice(enemy_hp)
